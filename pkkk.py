d102f813026f405faf6cc0defe5ae7999683f4b3
Kutyl87 <116507438+Kutyl87@users.noreply.github.com>
2022-11-23 17:46:55 +0100

    Create pkkk.py

---
+++ pkkk.py
@@ -0,0 +1,23 @@
+def muliply_word(word, sentence):
+    iter_word = iter((word*round(len(sentence)/len(word)))[:len(sentence)])
+    return "".join(next(iter_word)if sentence[i].isalnum() else sentence[i]
+                   for i in range(len(sentence)))
+
+
+def vigenere_cipher(word, sentence):
+    longer_word = muliply_word(word, sentence)
+    return "".join(chr(((ord(elem)-65+ord(element)-65) % 26)+65)
+                   if element.isalpha() else element for (elem, element)
+                   in zip(longer_word.upper(), sentence.upper()))
+
+
+
+def antyvigenere_cipher(word,sentence):
+    longer_word = muliply_word(word, sentence)
+    print(longer_word)
+    return "".join(chr((abs(ord(elem)-65-ord(element)-65) % 26)+65)
+                   if element.isalpha() else element for (elem, element)
+                   in zip(longer_word.upper(), sentence.upper()))
+print(vigenere_cipher("TAJNE", "TO JEST BARDZO TAJNY TEKST"))
+
+print(antyvigenere_cipher("TAJNE", "MO SRWM BJEHSO CNNGY CROLT"))
